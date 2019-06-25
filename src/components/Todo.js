c8d0319e993513063532d868ee6fab56c09e4cb8
hyoushke <hyoushke@gmail.com>
2019-06-25 22:25:27 +0800

    Created the ContentMenu and ContentContainer components

---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Todo extends Component{
+
+    render(){
+
+        return (
+        <div>
+            This is Todo
+        </div>
+        );
+    }
+}
+
+export default Todo;
